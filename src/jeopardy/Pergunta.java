1939794e6a17183f7e7285c86ff3231f533c879e
GREGORY CAMPOS BEVILÁQUA <gregbevilaqua@gmail.com>
2013-06-06 10:18:15 -0300

    Metodo Pergunta (Categoria)

--- src/jeopardy/Pergunta.java
+++ src/jeopardy/Pergunta.java
@@ -7,16 +7,25 @@ public class Pergunta {
 	private Categoria categoria;
 	private String caput;
 	private List<Resposta> respostas;
-	
+
 	public Pergunta(Categoria categoria, String caput) {
 		this.caput = caput;
 		this.categoria = categoria;
 		respostas = new ArrayList<Resposta>();
 	}
-	
+
 	public void addResposta(Resposta resposta) {
-		
+
+		if (!resposta.emUso()) {
+
+		}
+
 	}
-	
-	
+
+	public Categoria getCategoria() {
+
+		return categoria;
+
+	}
+
 }
--- src/jeopardy/Resposta.java
+++ src/jeopardy/Resposta.java
@@ -4,27 +4,27 @@ public class Resposta {
 	private final String texto;
 	private final boolean correta;
 	private boolean emUso;
-	
+
 	public Resposta(String texto, boolean correta, boolean emUso) {
 		this.texto = texto;
 		this.correta = correta;
 		this.emUso = emUso;
 	}
-	
+
 	public String getTexto() {
 		return texto;
 	}
-	
+
 	public boolean isCorreta() {
 		return correta;
 	}
-	
+
 	public boolean emUso() {
 		return emUso;
 	}
-	
+
 	public void usar() {
 		emUso = true;
 	}
-	
+
 }
