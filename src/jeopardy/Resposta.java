fd24c06d89730b112b45ce1ad6c3a4c866d2768d
Eduardo Mendes de Oliveira <edumendes@gmail.com>
2013-06-06 12:04:56 -0300

    Configuraçao do Jogo

--- src/jeopardy/Resposta.java
+++ src/jeopardy/Resposta.java
@@ -2,20 +2,14 @@ package jeopardy;
 
 public class Resposta {
 	private final String texto;
-	private final boolean correta;
 	private boolean emUso;
 
-	public Resposta(String texto, boolean correta) {
+	public Resposta(String texto) {
 		this.texto = texto;
-		this.correta = correta;
 	}
 
 	public String getTexto() {
 		return texto;
-	}
-
-	public boolean isCorreta() {
-		return correta;
 	}
 
 	public boolean emUso() {
