68009a58d9061493d709ba1529666353d08cc2eb
Eduardo Mendes de Oliveira <edumendes@gmail.com>
2013-06-06 10:39:14 -0300

    Inicio da lógica do jogo

--- src/jeopardy/Jogo.java
+++ src/jeopardy/Jogo.java
@@ -4,17 +4,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Jogo {
-	
+
 	private Map<Categoria, Pergunta> perguntas;
-	
-	public Jogo() {
+	private String nome;
+	private int maximoPerguntas;
+	private int totalPerguntas;
+
+	public Jogo(String nome) {
+		this(nome, 10);
+	}
+
+	public Jogo(String nome, int maximo) {
+		this.nome = nome;
 		perguntas = new HashMap<Categoria, Pergunta>();
+		maximoPerguntas = maximo;
 	}
-	
+
 	public void addPergunta(Pergunta pergunta) {
-		perguntas.put(pergunta.getCategoria(), pergunta);
+		if (totalPerguntas < maximoPerguntas) {
+			perguntas.put(pergunta.getCategoria(), pergunta);
+			totalPerguntas++;
+		}
 	}
-	
-	
-	
+
+	public static void main(String[] args) {
+		Jogo jogo = new Jogo("Perguntas e Respostas");
+	}
+
 }
